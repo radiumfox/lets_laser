56753f99b5c6482052f9ed75bab6f89228f0cbdd
Radium Fox <eteone.longa@gmail.com>
2021-10-11 03:39:21 +0300

    fix bug with mobile menu not scrolling

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,11 +1,27 @@
 const navMain = document.querySelector('.main-nav');
 const pageFooter = document.querySelector('.page-footer');
 
+const navToggle = document.querySelector('.main-nav__toggle');
+if (navMain) {
+  navToggle.addEventListener('click', () => {
+
+    if (!navMain.classList.contains('main-nav--opened')) {
+      navMain.classList.add('main-nav--opened');
+      body.classList.add('page-body--noscroll');
+    } else {
+      navMain.classList.remove('main-nav--opened');
+      body.classList.remove('page-body--noscroll');
+    }
+  });
+}
+
 const treatLinks = (link) => {
   link.addEventListener('click', (evt)=> {
     evt.preventDefault();
     const id = link.getAttribute('href');
     document.querySelector(id).scrollIntoView({block: 'start', behavior: 'smooth'});
+    body.classList.remove('page-body--noscroll');
+    navMain.classList.remove('main-nav--opened');
   });
 }
 
